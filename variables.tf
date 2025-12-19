10a92de2205743ad128237263c2feb6abad2e4b0
CyberDemons <103848881+Demon105856e@users.noreply.github.com>
2025-12-19 11:00:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,6 +10,10 @@ variable "ami_id" {
   default = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 Mumbai
 }
 
+variable "repo_name" {
+  type = string
+}
+
 variable "allowed_ports" {
   type    = list(number)
   default = [22, 80, 443]
