e8ca8e8c75da628496b97d90adbdf4a3d8821427
CyberDemons <103848881+Demon105856e@users.noreply.github.com>
2025-12-19 10:43:21 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "nginx_sg" {
+  name   = "nginx-sg"
+  vpc_id = aws_vpc.nginx_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
